fdf38f7eee2eb411c0d9faa077530ef53c4f62d5
Egor Shitikov <egorshitikov@gmail.com>
2022-02-17 20:36:41 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,138 @@
+## <https://www.terraform.io/docs/providers/azurerm/index.html>
+
+# Configure the Microsoft Azure Provider
+
+provider "azurerm" {
+  version = "=2.5.0"
+  features {}
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
+resource "azurerm_resource_group" "rg" {
+  name     = "TerraformMagento"
+  location = "eastus"
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/availability_set.html>
+resource "azurerm_availability_set" "MagentoAset" {
+  name                = "magento-aset"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/virtual_network.html>
+resource "azurerm_virtual_network" "vnet" {
+  name                = "Magento"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/subnet.html> 
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "eastus"
+    resource_group_name          = azurerm_resource_group.rg.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Magento"
+    }
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/network_interface.html>
+resource "azurerm_network_interface" "magento" {
+  name                = "magento-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "MagentoNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule {
+        name                       = "Allow-All"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.magento.id
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "magento_ssh" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+output "tls_private_key" { 
+    value = tls_private_key.magento_ssh.private_key_pem 
+    sensitive = true
+}
+# To get the corresponding private key, run terraform output -raw tls_private_key
+
+## <https://www.terraform.io/docs/providers/azurerm/r/windows_virtual_machine.html>
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "Magento-Web"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "magento2"
+  ##Change Password or better disable Password and use SSH key
+  admin_password      = "DSF@SDfsd3242dgdfg34sdfsdfwer!2323"
+  #disable_password_authentication = true
+  
+  # admin_ssh_key {
+  #      username       = "azureuser"
+  #      public_key     = tls_private_key.magento_ssh.public_key_openssh
+  #  }
+    
+  availability_set_id = azurerm_availability_set.MagentoAset.id
+  network_interface_ids = [
+    azurerm_network_interface.magento.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+    computer_name  = "magento"
+
+}
