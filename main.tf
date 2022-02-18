0eb7a23dc1cf1347993d5edb5ebcff65693152fa
Egor Shitikov <egorshitikov@gmail.com>
2022-02-17 21:13:23 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,6 +107,16 @@ output "tls_private_key" {
 }
 # To get the corresponding private key, run terraform output -raw tls_private_key
 
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.rg.name
+    }
+
+    byte_length = 8
+}
+
 # Create storage account for boot diagnostics
 resource "azurerm_storage_account" "magento" {
     name                        = "diag${random_id.randomId.hex}"
