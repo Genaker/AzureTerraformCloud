cc36230a65622015990b4cc28118bbf6b36a3ced
Egor Shitikov <egorshitikov@gmail.com>
2022-02-17 20:57:18 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 ## <https://www.terraform.io/docs/providers/azurerm/index.html>
 
 # Configure the Microsoft Azure Provider
-
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
 provider "azurerm" {
-  version = "=2.5.0"
   features {}
 }
 
