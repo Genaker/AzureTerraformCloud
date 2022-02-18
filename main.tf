d45aff2799a30405c87ae9e26bfffdf185edd011
Egor Shitikov <egorshitikov@gmail.com>
2022-02-17 23:16:52 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,10 +52,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes       = ["10.0.2.0/24"]
-  
-  tags = {
-        environment = "Terraform Magento"
-    }
 }
 
 # Create public IPs
