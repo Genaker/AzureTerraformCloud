8e3b823fe4e38d463156031c781af3ca59b5d401
Egor Shitikov <egorshitikov@gmail.com>
2022-02-17 21:03:33 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes       = ["10.0.2.0/24"]
 }
 
 # Create public IPs
